2c369e197bef88ba6d63a696c7251df4aaa29c1e
SKishore-Kumar <Kk0820763@gmail.com>
2022-03-22 17:37:30 +0000

    user.java

--- springapp/src/main/java/com/examly/springapp/SpringappApplication.java
+++ springapp/src/main/java/com/examly/springapp/SpringappApplication.java
@@ -3,6 +3,7 @@ package com.examly.springapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SpringappApplication {
 
