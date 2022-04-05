b0e9866ee207652c28e2c59a745955a2b6c70969
RexMilton <rexmilton5@gmail.com>
2022-04-05 13:19:08 +0000

    m

--- reactapp/src/App.test.js
+++ reactapp/src/App.test.js
@@ -1,4 +1,4 @@
-htttimport { render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
